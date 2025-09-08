b60e3e94354c2cf17503ea93a2c4cfcea76022b9
Moises Velasquez <moises007@gmail.com>
2025-09-08 17:04:34 -0500

    connected and disconnected

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,8 +1,21 @@
-import { Manager } from "socket.io-client";
+import { Manager, Socket } from "socket.io-client";
 
 export const connectToServer = () => {
   const url = "http://localhost:3000/socket.io/socket.io.js";
   const manager = new Manager(url);
 
   const socket = manager.socket("/");
+  addListeners(socket);
+};
+
+const addListeners = (socket: Socket) => {
+  const serverStatusLabel = document.querySelector("#server-status")!;
+
+  socket.on("connect", () => {
+    console.log("connected");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
 };
