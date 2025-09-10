5797dd4e9b054f3d7b4cc86c9b769cfc3b3c0dd1
Moises Velasquez <moises007@gmail.com>
2025-09-09 20:56:29 -0500

    socket listen and emit server

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <h1>Websocket - Client</h1>
     <span id="server-status">offlilne</span>
     <ul id="clients-ul"></ul>
+    <form id="message-form">
+      <input placeholder="message" type="text" id="message-input" />
+    <form/>
+
+    <h3>Messages</h3>
+    <ul id="messages-ul"></ul>
   </div>
 `;
 connectToServer();
